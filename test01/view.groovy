e0dc627800718fc1e57b41ebfca5852c97f48009
pmszmaglinski <pm.szmaglinski@gmail.com>
2021-11-04 14:21:55 +0100

    Create view.groovy

---
+++ test01/view.groovy
@@ -0,0 +1,48 @@
+listView("testView01") {
+	jobs {
+		name("Test-job01")
+		name("Test-job03")
+		name("Test-job02")
+	}
+	columns {
+		status()
+		weather()
+		name()
+		lastSuccess()
+		lastFailure()
+		lastDuration()
+		buildButton()
+	}
+}
+
+listView("testView02") {
+	jobs {
+		name("Test-job02")
+
+	}
+	columns {
+		status()
+		weather()
+		name()
+		lastSuccess()
+		lastFailure()
+		lastDuration()
+		buildButton()
+	}
+}
+
+listView("testView04") {
+	jobs {
+		name("Test-job04")
+
+	}
+	columns {
+		status()
+		weather()
+		name()
+		lastSuccess()
+		lastFailure()
+		lastDuration()
+		buildButton()
+	}
+}
