96ef43b654bc55137da598de492fb58221acf2d6
piotr.szmaglinski <piotr.szmaglinski@jeppesen.com>
2021-11-15 11:49:18 +0100

    Testing

--- test01/jobs.groovy
+++ test01/jobs.groovy
@@ -1,32 +1,41 @@
 job('Test-job01') {
   label('master')
+
   steps {
   
     shell('echo $JOB_NAME-$BUILD_NUMBER')
+    shell('echo blablabla')
   }
 }
 
 
 job('Test-job02') {
+  label('master')
 
   steps {
   
     shell('echo $JOB_NAME')
+    shell('echo blablabla')
+
   }
 }
 
 job('Test-job03') {
+  label('master')
 
   steps {
   
     shell('echo $JOB_NAME')
+    shell('echo blablabla')
   }
 }
 
 job('Test-job04') {
+  label('master')
 
   steps {
   
     shell('echo $JOB_NAME')
+    shell('echo blablabla')
   }
 }
