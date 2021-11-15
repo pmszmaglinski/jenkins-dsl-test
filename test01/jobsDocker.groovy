605367efb66edddb0b4c72a25876466fb3e9ce43
piotr.szmaglinski <piotr.szmaglinski@jeppesen.com>
2021-11-15 12:19:44 +0100

    Testing

--- test01/jobsDocker.groovy
+++ test01/jobsDocker.groovy
@@ -3,7 +3,6 @@ pipelineJob("TestJob") {
     keepDependencies(false)
     definition {
         cpsScm {
-            scm { none() }
             """podTemplate (containers: [containerTemplate(name: 'alpine', image: 'alpine:latest', command: 'sleep', args: '99d'),
                                          containerTemplate(name: 'centos', image: 'centos:7', command: 'sleep', args: '99d')
                                         ]
