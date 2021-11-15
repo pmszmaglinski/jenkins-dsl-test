1fc3283454da643c09e9ed1d234ed8b863309d4d
piotr.szmaglinski <piotr.szmaglinski@jeppesen.com>
2021-11-15 12:04:21 +0100

    Testing

---
+++ test01/jobs-docker.groovy
@@ -0,0 +1,35 @@
+pipelineJob("TestJob") {
+    description()
+    keepDependencies(false)
+    definition {
+        cpsScm {
+            """podTemplate (containers: [containerTemplate(name: 'alpine', image: 'alpine:latest', command: 'sleep', args: '99d'),
+                                         containerTemplate(name: 'centos', image: 'centos:7', command: 'sleep', args: '99d')
+                                        ]
+                            ) {
+                
+    node(POD_LABEL) {
+            
+        stage('Jnlp') {
+            println 'cycki'
+            sh 'echo \$JAVA_OPTS'
+        }
+        
+        stage('Alpine') {
+            container('alpine') {
+                sh 'cat /etc/os-release'
+                sh 'echo \$JAVA_OPTS'
+            }
+        }
+        
+        stage('Centos') {
+            container('centos') {
+                sh 'cat /etc/os-release'
+                sh 'echo \$JAVA_OPTS'
+            }
+        }
+    }    
+}"""		}
+    }
+    disabled(false)
+}
