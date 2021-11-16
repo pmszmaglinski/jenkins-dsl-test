bc9126525db3dcd08b675972edf389ec8ca42d7d
piotr.szmaglinski <piotr.szmaglinski@jeppesen.com>
2021-11-16 14:53:42 +0100

    Testing

---
+++ test01/jobMultibranch.groovy
@@ -0,0 +1,27 @@
+multibranchPipelineJob('QualityAnalysis-Test') {
+    displayName('QualityAnalysis-Test-DisplayName')
+    branchSources {
+        git {
+            id('123456789')
+            remote('https://dat.xxxxx.com/git/scm/da/xxx-repo.git')
+            credentialsId('xxx_service')
+            includes('(master|develop)')
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('jenkins/jobs/Jenkinsfile')
+        }
+    }
+    configure {
+        it / disabled << 'true'
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            daysToKeep(5)
+        }
+    }
+    triggers {
+        periodic(5)
+    }
+}
